b76a15e1f2ba787e3e6a1f329d9b9eda41f6562e
Subin20 <86279871+Subin20@users.noreply.github.com>
2024-07-02 11:42:51 +0530

    Third time
    I able to extract data and convert to csv file also.

---
+++ Carbon38/Carbon38/spiders/dynanic_way.py
@@ -0,0 +1,13 @@
+# from requests_html import HTMLSession
+
+# url ='https://carbon38.com/en-in/collections/tops?filter.p.m.custom.available_or_waitlist=1'
+
+# s = HTMLSession()
+
+# r = s.get(url)
+
+# r.html.render(sleep=1)
+
+# products = r.html.xpath('//*[@id="shopify-section-template--17143390470333__main"]/section/div/div[2]/div[2]/div[2]/div[1]',first = True)
+
+# print(products.absolute_links)
--- Carbon38/Carbon38/spiders/products_spider.py
+++ Carbon38/Carbon38/spiders/products_spider.py
@@ -10,7 +10,6 @@ class CrawlingSpider(scrapy.Spider):
     start_urls = ["https://carbon38.com/collections/tops?filter.p.m.custom.available_or_waitlist=1"]
 
 
-    #
     # rules = (
     #     Rule(LinkExtractor(allow="catalogue/category")),
     #     Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item")
@@ -31,16 +30,40 @@ class CrawlingSpider(scrapy.Spider):
     def parse(self,response):
          products = response.css('div.ProductItem__Info')
          for product in products:
+            
+            # relative_url=product.css('h2.ProductItem__Title a').attrib['href']
+            # item_url = 'https://carbon38.com' + relative_url
+            # yield response.follow(item_url,callback = self.parse_tem_page)
+
+
+
             yield {
 
                 "name" : product.css("h2.ProductItem__Title a::text").get(),
                 "price" : product.css("span.ProductItem__Price").get().replace('<span class="ProductItem__Price Price">','').replace('</span>',''),
                 "url" : product.css('h2.ProductItem__Title a').attrib['href'],
+            
+            }                                                    
+            
+            next_page = response.css('[rel="next"] ::attr(href)').get()
+           
+
+            if next_page is not None:
+                next_page_url = 'https://carbon38.com' + next_page
+                yield response.follow(next_page_url,callback = self.parse)
 
 
-            }
+    # def parse_item_page(self,response):
+          #  c=response.css('div.shopify-section').get()
+        
 
-            # next_page = response.css('[rel="next"] ::attr(href)').get()
-            # if next_page is not None:
-            #     next_page_url = 'https://carbon38.com/collections/tops?filter.p.m.custom.available_or_waitlist=1' + next_page
-            #     yield response.follow(next_page_url,callback=self.parse)
+    #     # for c in col:
+    #     #     relative_url=response.css('h2.ProductItem__Title a').attrib['href']
+        
+    #     yield {
+
+    #             "name" : response.css('h1.ProductMeta__Title ::text').get(),
+    #             "price" : response.css('div.ProductMeta__PriceList span.ProductMeta__Price::text').get(),
+    #             "url" : response.css('h2.ProductMeta__Vendor a').attrib['href'] ,             
+    #             "color": response.css('span.ProductForm__Label span.ProductForm__SelectedValue::text').get() ,   
+    #         }
